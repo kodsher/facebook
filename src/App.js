2918db0b66a9c06ee1c9e8289805961fdde6302e
kodsher <kodysherman@gmail.com>
2025-11-04 11:35:25 -0600

    Add React app that displays Hello message

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontSize: '48px',
+      fontFamily: 'Arial, sans-serif'
+    }}>
+      Hello!
+    </div>
+  );
+}
+
+export default App;
